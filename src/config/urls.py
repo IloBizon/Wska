8d10cbdb4e8d3b1b33aec805dd7d6454b59171a3
first_in_python <ntr.0707@mail.ru>
2025-01-13 14:52:24 +0300

    Главная страница

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render, redirect
+def index(request):
+    return render(request, 'acc/index.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('polls/', include('polls.urls')),
     path('account/', include('account.urls')),
+    path("", index)
+
 ]
