9d29d60861da6d67db1a5fc8a5b7f530723162b0
WhiteSakura1 <Kostes4354@gmail.com>
2025-01-13 15:01:36 +0300

    Fix user_login

--- src/account/views.py
+++ src/account/views.py
@@ -17,12 +17,15 @@ def user_login(request):
     if request.method == 'POST':
         form = UserLogForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('index')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
     else:
         form = UserLogForm()
     return render(request, 'acc/login.html', {'form': form})
-
 def user_logout(request):
     logout(request)
     return redirect('login')
